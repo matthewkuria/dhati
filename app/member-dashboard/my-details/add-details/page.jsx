72cd1abcf82deab5c4579826040075c9bf4b862c
Matthewkuria <mathewkwachira@gmail.com>
2025-04-27 23:11:08 +0300

    Remove the fonts.ts

--- app/member-dashboard/my-details/add-details/page.jsx
+++
@@ -1,7 +0,0 @@
-import AddDetailsForm from "@/app/ui/member-dashboard/my-details/add-details/add-details-form";
-
-export default function Page() {
-    return (
-        <AddDetailsForm />
-    )
-}
